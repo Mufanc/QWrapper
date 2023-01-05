06e6391cdeafdac1624bab39c3a8b1bc09e1233a
Mufanc <mufanc.xyz@gmail.com>
2023-01-06 01:39:20 +0800

    feat: 共享 Downloads 目录

--- __patch__/wrap.sh
+++ __patch__/wrap.sh
@@ -3,6 +3,8 @@ exec bwrap --unshare-all --share-net \
   --dev-bind / / \
   --proc /proc \
   --ro-bind "$HOME" "$HOME" \
+  --bind "$HOME/Downloads" "$HOME/Downloads" \
+  --tmpfs "$HOME/.config" \
   --bind "$HOME/.config/QQ" "$HOME/.config/QQ" \
   --setenv LD_PRELOAD /opt/QQ/__patch__/libhook.so \
   --chdir "$HOME" \
